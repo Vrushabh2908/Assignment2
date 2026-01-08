9239888395e121a40f7c8b8bc9474538165017fa
vrushh <vrushabhdeokar007@gmail.com>
2026-01-08 21:43:33 +0530

    Update task1_evenodd.py
    Perform number even or odd

--- task1_evenodd.py
+++ task1_evenodd.py
@@ -1,8 +1,8 @@
-#Check if a number is even or odd
+# Task 1: Check if a Number is Even or Odd
 
 num = int(input("Enter an integer number: "))
 
 if num % 2 == 0:
-    print("{num} is an even number.")
+    print(f"{num} is an even number.")
 else:
-    print("{num} is an odd number.")
+    print(f"{num} is an odd number.")
